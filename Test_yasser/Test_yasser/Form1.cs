082ace4b4d2e44a8bfadb7bf0f966dd40e86122f
yasser adnan mohammad shaweesh <yasser_just_for_me@hotmail.com>
2015-10-24 16:37:13 +0300

    add mbox to button

--- Test_yasser/Test_yasser/Form1.cs
+++ Test_yasser/Test_yasser/Form1.cs
@@ -16,5 +16,10 @@ namespace Test_yasser
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
